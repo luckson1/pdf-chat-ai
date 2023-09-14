24700f764cbc299594c4dd2d71a7a27a266cfb22
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-15 00:26:44 +0300

    fix: not needed

--- src/scripts/pinecone-prepare-docs.ts
+++ src/scripts/pinecone-prepare-docs.ts
@@ -1,21 +1,22 @@
-import { getChunkedDocsFromPDF } from "@/lib/pdf-loader";
-import { pineconeEmbedAndStore } from "@/lib/vector-store";
-import { getPineconeClient } from "@/lib/pinecone-client";
+// import { getChunkedDocsFromUnstrucuted } from "@/lib/loaders";
+// import { pineconeEmbedAndStore } from "@/lib/vector-store";
+// import { getPineconeClient } from "@/lib/pinecone-client";
+// import { env } from "@/lib/env";
 
-// This operation might fail because indexes likely need
-// more time to init, so give some 5 mins after index
-// creation and try again.
-(async () => {
-      console.log("here")
-  try {
+// // This operation might fail because indexes likely need
+// // more time to init, so give some 5 mins after index
+// // creation and try again.
+// (async () => {
+//       console.log("here")
+//   try {
 
-    const pineconeClient = await getPineconeClient();
-    console.log("Preparing chunks from PDF file");
-    const docs = await getChunkedDocsFromPDF();
-    console.log(`Loading ${docs.length} chunks into pinecone...`);
-    await pineconeEmbedAndStore(pineconeClient, docs);
-    console.log("Data embedded and stored in pine-cone index");
-  } catch (error) {
-    console.error("Init client script failed ", error);
-  }
-})();
+//     const pineconeClient = await getPineconeClient();
+//     console.log("Preparing chunks from PDF file");
+//     const docs = await getChunkedDocsFromUnstrucuted(env.PDF_PATH);
+//     console.log(`Loading ${docs.length} chunks into pinecone...`);
+//     await pineconeEmbedAndStore(pineconeClient, docs, env.PDF_PATH);
+//     console.log("Data embedded and stored in pine-cone index");
+//   } catch (error) {
+//     console.error("Init client script failed ", error);
+//   }
+// })();
