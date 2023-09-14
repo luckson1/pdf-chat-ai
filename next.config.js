6b52617237bbee407e1f314022bacbb8dc5c0435
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-14 16:38:36 +0300

    fix: image domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com']
+  },
   swcMinify: true,
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
