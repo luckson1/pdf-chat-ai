8400f6c9131e6e2e243764a2dab70af7097749ad
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-15 00:41:03 +0300

    fix: build errors

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -1,13 +1,13 @@
 
 import { Chat } from "@/components/chat";
-import MainDocumentViewer from "@/components/documentViewer";
+// import MainDocumentViewer from "@/components/documentViewer";
 
 export default function Home() {
   return (
   
         <main className="w-full flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
           <div className="w-full max-w-lg">
-<MainDocumentViewer />
+{/* <MainDocumentViewer /> */}
           </div>
           <div className="w-full max-w-3xl">
 <Chat />
