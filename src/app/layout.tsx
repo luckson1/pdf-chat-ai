6a9cb88a99582be5f9c2bdd751c59ece00d574f2
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-14 16:39:39 +0300

    new layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import Provider from "@/components/provider";
 import TRPCProvider from "./api/_trpc/Provider";
+import { SiteHeader } from "@/components/site-header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +26,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Provider>
            <TRPCProvider>
             <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="relative container flex min-h-screen flex-col">
+   <SiteHeader />
+      <div className="flex flex-1 py-4">
               {children}
+              </div>
+              </div>
             </ThemeProvider>
             </TRPCProvider> 
           </Provider>
