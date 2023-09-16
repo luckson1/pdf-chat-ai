d6ed8c60c4aa94089c92c256a1cfe00cbab26cc4
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-16 04:40:51 +0300

    chat appear first

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -5,7 +5,7 @@ import MainDocumentViewer from "@/components/documentViewer";
 export default function Home() {
   return (
   
-        <main className="w-full flex flex-col md:flex-row space-y-5 md:space-y-0 md:space-x-5">
+        <main className="w-full flex flex-col-reverse md:flex-row space-y-5 md:space-y-0 md:space-x-5">
           <div className="w-full max-w-3xl h-auto">
 <MainDocumentViewer />
           </div>
