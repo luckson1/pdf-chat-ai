e9a6094ee5bd1a544a6964322069c91bdde90f00
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-16 16:57:11 +0300

    fix: style

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -3,6 +3,4 @@ import { inngest } from "../../../inngest/client";
 import { creates3Embeddings } from "@/inngest/functions";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(inngest, [creates3Embeddings
-
-]);
+export const { GET, POST, PUT } = serve(inngest, [creates3Embeddings]);
