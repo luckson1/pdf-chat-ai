73b66441801d2383526d10c9d727cc4683bc07e8
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-16 11:52:30 +0300

    border radius

--- src/components/s3Loader.tsx
+++ src/components/s3Loader.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { api } from "@/app/api/_trpc/client";
-import { PDFViewer } from "./viewers";
 import { memo, useEffect, useState } from "react";
 import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
  function Viewer({signedUrl, type }: { signedUrl: string, type:string }) {
@@ -10,7 +8,7 @@ import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 
   ];
 
-  return <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} className="h-auto min-h-[90vh] w-full"/>;
+  return <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} className="h-auto min-h-[90vh] w-full rounded-lg"/>;
   
 }
 
