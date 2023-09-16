2dcc97c708dddd54164f78a17389153c8dc724be
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-16 11:51:14 +0300

    clean up

--- src/components/s3Loader.tsx
+++ src/components/s3Loader.tsx
@@ -10,64 +10,8 @@ import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
 
   ];
 
-  return <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} className="h-auto min-h-[70vh] w-full"/>;
+  return <DocViewer documents={docs} pluginRenderers={DocViewerRenderers} className="h-auto min-h-[90vh] w-full"/>;
   
-//   switch (data.type) {
-//     // case 'pdf':
-//         return <PDFViewer data={data} />;
-//     case "docx":
-//       return <DocxViewer signedUrl={data.fileUrl} />;
-//     case "csv":
-//       return <CSVViewer signedUrl={data.fileUrl} />;
-//     case "ppt":
-//       return (
-//         <div>
-//           PowerPoint presentations are not supported for direct viewing. Please
-//           download to view.
-//         </div>
-//       );
-//     default:
-//       return <div>Unsupported format.</div>;
-//   }
-
-// const [fileData, setFileData] = useState<Uint8Array | null>(null);
-// const [loading, setLoading] = useState(false);
-// const [error, setError] = useState<string | null>(null);
-
-// useEffect(() => {
-//     async function fetchUint8ArrayFromS3(url: string) {
-//         try {
-//           setLoading(true)
-//             const response = await fetch(url);
-
-//             if (!response.ok) {
-//                 throw new Error(`Failed to fetch file. Status: ${response.status}`);
-//             }
-
-//             const blob = await response.blob();
-//             const arrayBuffer = await blob.arrayBuffer();
-//             setFileData(new Uint8Array(arrayBuffer));
-//         } catch (err) {
-//           //@ts-ignore
-//             setError(err.message);
-//             console.log("here", err)
-//         } finally {
-//             setLoading(false);
-//         }
-//     }
-
-//     fetchUint8ArrayFromS3(signedUrl);
-// }, [signedUrl]);
-
-// if (loading) {
-//     return <div>Loading...</div>;
-// }
-
-// if (error) {
-//     return <div>Error: {error}</div>;
-// }
-// if (!fileData) return null
-// return <PDFViewer data={fileData} />;
 }
 
 export const  DocumentViewer = memo(Viewer)
