daabd9ccfb6ebdebed2c366b0941ac90a2565a34
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-18 02:05:32 +0300

    use openai api

--- src/app/api/upload_audio/route.ts
+++ src/app/api/upload_audio/route.ts
@@ -1,3 +1,4 @@
+import { env } from "@/lib/env.mjs";
 import fs from "fs";
 import { OpenAIWhisperAudio } from "langchain/document_loaders/fs/openai_whisper_audio";
 import { NextResponse } from "next/server";
@@ -10,16 +11,25 @@ try {
     for (const formDataEntryValue of formDataEntryValues) {
         formDataEntryValue
       if (typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue) {
-        const file = formDataEntryValue as unknown as Blob;
-        const buffer = Buffer.from(await file.arrayBuffer());
-        fs.writeFileSync(`/tmp/${file.name}`, buffer);
-  console.log(file)
-      const loader = new OpenAIWhisperAudio(`/tmp/${file.name}`);
+        const res = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+            headers: {
+              Authorization: `Bearer ${env.OPENAI_API_KEY}`,
+            },
+            method: "POST",
+            body: formData,
+          });
+          const data = await res.json();
+          console.log(data.text)
+        // const file = formDataEntryValue as unknown as Blob;
+        // const buffer = Buffer.from(await file.arrayBuffer());
+        // fs.writeFileSync(`/tmp/${file.name}`, buffer);
+ 
+    //   const loader = new OpenAIWhisperAudio(`/tmp/${file.name}`);
   
-      const docs = await loader.load();
+    //   const docs = await loader.load();
       
-      console.log(docs);
-      return docs
+    //   console.log(docs);
+    //   return docs
       }
     }
     return NextResponse.json({ success: true });
