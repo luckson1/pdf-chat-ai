1cf5539347f730fa3a327523568774a50d9a507f
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-18 02:59:33 +0300

    rename field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function DocumentPage() {
   const [audio, setAudio] = useState<File[]>([]);
   const [url, setUrl] = useState<string>();
   const [loading, setLoading] = useState(false);
-  const uploadToS3 = async (files:  File[]) => {
+  const uploadToS3 = async (files: File[]) => {
     if (!files || files.length <= 0) {
       return null;
     }
@@ -40,23 +40,23 @@ export default function DocumentPage() {
     const d = await axios.put(uploadUrl, files[0]);
     return { key, name, type };
   };
-  const ctx=api.useContext()
-  const {  mutate: addDoc } = api.documents.addDoc.useMutation( {
-    onSuccess: ()=> {
-      ctx.documents.getAll.invalidate()
-    }
+  const ctx = api.useContext();
+  const { mutate: addDoc } = api.documents.addDoc.useMutation({
+    onSuccess: () => {
+      ctx.documents.getAll.invalidate();
+    },
   });
-  const {  mutate: addWebDoc } = api.documents.addWebDoc.useMutation( {
-    onSuccess: ()=> {
-      ctx.documents.getAll.invalidate()
-    }
+  const { mutate: addWebDoc } = api.documents.addWebDoc.useMutation({
+    onSuccess: () => {
+      ctx.documents.getAll.invalidate();
+    },
   });
-  const {  mutate: addAudio } = api.documents.addAudioDoc.useMutation( {
-    onSuccess: ()=> {
-      ctx.documents.getAll.invalidate()
-    }
+  const { mutate: addAudio } = api.documents.addAudioDoc.useMutation({
+    onSuccess: () => {
+      ctx.documents.getAll.invalidate();
+    },
   });
-  const urlSchema= z.string().url()
+  const urlSchema = z.string().url();
   const handleSubmitDocs = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
@@ -68,150 +68,149 @@ export default function DocumentPage() {
       }
       addDoc(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
       setLoading(false);
-      setDocs([])
+      setDocs([]);
     }
   };
   const handleSubmitAudio = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       setLoading(true);
-   const formData= new FormData()
-   audio.forEach((a, i)=> {
-    formData.append("model", "whisper-1");
-    formData.append(a.name, a)
-   
+      const formData = new FormData();
+      audio.forEach((a, i) => {
+        formData.append("file", a);
+      });
 
-   })
-   await axios.post('api/upload_audio', formData)
+      //  await axios.post('api/upload_audio', formData)
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
       setLoading(false);
-      setAudio([])
+      setAudio([]);
     }
   };
   const handleSubmitWebPage = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       setLoading(true);
-  
-   const validUrl = urlSchema.parse(url);
-   addWebDoc({url:validUrl})
+
+      const validUrl = urlSchema.parse(url);
+      addWebDoc({ url: validUrl });
     } catch (error) {
-      console.log(error)
-    } finally{
-      setLoading(false)
-      setUrl('')
+      console.log(error);
+    } finally {
+      setLoading(false);
+      setUrl("");
     }
   };
   const { data: docsData, isLoading } = api.documents.getAll.useQuery();
   return (
     <div className="w-full h-fit flex flex-col md:flex-row space-x-0 md:space-x-5 space-y-5 md:space-y-0">
-        <Tabs defaultValue="docs" className="w-full max-w-sm">
-      <TabsList className="grid w-full grid-cols-3">
-      <TabsTrigger value="docs">Documents</TabsTrigger>
-        <TabsTrigger value="url"> Online Article</TabsTrigger>
-        <TabsTrigger value="audio">Audio</TabsTrigger>
-        
-      </TabsList>
-      <TabsContent value="docs">
-      <Card className="w-full max-w-sm h-fit min-h-[500px]">
-        <CardHeader>
-          <CardTitle>Upload a study material</CardTitle>
-          <CardDescription>
-            Upload a pdf, word, text, epub or power point
-          </CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col space-y-5 py-5">
-            <form
-              className="flex flex-col space-y-5"
-              onSubmit={(e) => handleSubmitDocs(e)}
-              id="docs"
-            >
-              <Dropzone files={docs} setFiles={setDocs} audio={false}/>
-              <Button type="submit" disabled={docs.length <= 0} className="w-full max-w-xs">
-              {loading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          
-            )}
-                chat with your document
-              </Button>
-            </form>
-          
-           
-          </CardContent>
-      
-      </Card>
-      </TabsContent>
-      <TabsContent value='url'>
-      <Card className="w-full max-w-sm h-fit min-h-[500px]">
-          <CardHeader>
-            <CardTitle>
-Link to your online study material
-            </CardTitle>
-            <CardDescription>
-Provide a working  link to a blog or news article (online pdfs are not valid)
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-          <form
-              onSubmit={(e) => {
-                handleSubmitWebPage(e)
-              }}
-              className="flex w-full  max-w-sm items-center space-y-5 mt-10  flex-col"
-            >
-              <div className="w-full space-y-2">
-                <Label htmlFor="url">Add url to your source</Label>
-                <Input
-                  type="text"
-                  placeholder="https://..."
-                  value={url}
-                  onChange={(e) => setUrl(e.target.value)}
-                />
-              </div>
-              <Button className="mt-8 w-full max-w-xs" type="submit" disabled={!urlSchema.safeParse(url).success} >
-              {loading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          
-            )}
-                Chat with your web page
-              </Button>
-            </form>
-          </CardContent>
-        </Card>
-
-      </TabsContent>
-      <TabsContent value="audio">
-      <Card className="w-full max-w-sm h-fit min-h-[500px]">
-        <CardHeader>
-          <CardTitle>Upload an audio recording</CardTitle>
-          <CardDescription>
-            Record your professor lecture and chat with him/her later
-          </CardDescription>
-          </CardHeader>
-          <CardContent className="flex flex-col space-y-5 py-5">
-            <form
-              className="flex flex-col space-y-5"
-              onSubmit={(e) => handleSubmitAudio(e)}
-              id="audio"
-            >
-              <Dropzone files={audio} setFiles={setAudio} audio={true}/>
-              <Button type="submit" disabled={audio.length <= 0} className="max-w-xs w-full">
-            {loading && (  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          
-          )}
-              Chat with your web page
-              </Button>
-            </form>
-          
-           
-          </CardContent>
-      
-      </Card>
-      </TabsContent>
+      <Tabs defaultValue="docs" className="w-full max-w-sm">
+        <TabsList className="grid w-full grid-cols-3">
+          <TabsTrigger value="docs">Documents</TabsTrigger>
+          <TabsTrigger value="url"> Online Article</TabsTrigger>
+          <TabsTrigger value="audio">Audio</TabsTrigger>
+        </TabsList>
+        <TabsContent value="docs">
+          <Card className="w-full max-w-sm h-fit min-h-[500px]">
+            <CardHeader>
+              <CardTitle>Upload a study material</CardTitle>
+              <CardDescription>
+                Upload a pdf, word, text, epub or power point
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col space-y-5 py-5">
+              <form
+                className="flex flex-col space-y-5"
+                onSubmit={(e) => handleSubmitDocs(e)}
+                id="docs"
+              >
+                <Dropzone files={docs} setFiles={setDocs} audio={false} />
+                <Button
+                  type="submit"
+                  disabled={docs.length <= 0}
+                  className="w-full max-w-xs"
+                >
+                  {loading && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  chat with your document
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="url">
+          <Card className="w-full max-w-sm h-fit min-h-[500px]">
+            <CardHeader>
+              <CardTitle>Link to your online study material</CardTitle>
+              <CardDescription>
+                Provide a working link to a blog or news article (online pdfs
+                are not valid)
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form
+                onSubmit={(e) => {
+                  handleSubmitWebPage(e);
+                }}
+                className="flex w-full  max-w-sm items-center space-y-5 mt-10  flex-col"
+              >
+                <div className="w-full space-y-2">
+                  <Label htmlFor="url">Add url to your source</Label>
+                  <Input
+                    type="text"
+                    placeholder="https://..."
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                  />
+                </div>
+                <Button
+                  className="mt-8 w-full max-w-xs"
+                  type="submit"
+                  disabled={!urlSchema.safeParse(url).success}
+                >
+                  {loading && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Chat with your web page
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="audio">
+          <Card className="w-full max-w-sm h-fit min-h-[500px]">
+            <CardHeader>
+              <CardTitle>Upload an audio recording</CardTitle>
+              <CardDescription>
+                Record your professor lecture and chat with him/her later
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col space-y-5 py-5">
+              <form
+                className="flex flex-col space-y-5"
+                onSubmit={(e) => handleSubmitAudio(e)}
+                id="audio"
+              >
+                <Dropzone files={audio} setFiles={setAudio} audio={true} />
+                <Button
+                  type="submit"
+                  disabled={audio.length <= 0}
+                  className="max-w-xs w-full"
+                >
+                  {loading && (
+                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Chat with your web page
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
       </Tabs>
       <div className="w-full max-w-4xl flex flex-col space-y-5">
         {isLoading &&
@@ -222,19 +221,20 @@ Provide a working  link to a blog or news article (online pdfs are not valid)
             ))}
         {(!docsData || docsData.length <= 0) && !isLoading ? (
           <Card className="w-full">
-          
             <CardHeader>No Documents</CardHeader>
           </Card>
-        ) : docsData  && !isLoading ? (docsData.map(doc=> (
-          <Link key={doc.id} href={{     pathname: '/documents/[id]',
-          query: { id: doc.id }}}>
-          <Card className="w-full">
-<CardHeader>
-  {doc.name}
-</CardHeader>
-          </Card>
-          </Link>
-        ))): null}
+        ) : docsData && !isLoading ? (
+          docsData.map((doc) => (
+            <Link
+              key={doc.id}
+              href={{ pathname: "/documents/[id]", query: { id: doc.id } }}
+            >
+              <Card className="w-full">
+                <CardHeader>{doc.name}</CardHeader>
+              </Card>
+            </Link>
+          ))
+        ) : null}
       </div>
     </div>
   );
