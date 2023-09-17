7efa70f8a194eeb4bb7036725d3f803a1d0f8535
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-18 01:14:47 +0300

    feat: new route to upload audio

---
+++ src/app/api/upload_audio/route.ts
@@ -0,0 +1,27 @@
+import fs from "fs";
+import { OpenAIWhisperAudio } from "langchain/document_loaders/fs/openai_whisper_audio";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+try {
+    const formData = await req.formData();
+    const formDataEntryValues = Array.from(formData.values());
+    for (const formDataEntryValue of formDataEntryValues) {
+      if (typeof formDataEntryValue === "object" && "arrayBuffer" in formDataEntryValue) {
+        const file = formDataEntryValue as unknown as Blob;
+      //   const buffer = Buffer.from(await file.arrayBuffer());
+      //   fs.writeFileSync(`public/${file.name}`, buffer);
+  
+      const loader = new OpenAIWhisperAudio(file);
+  
+      const docs = await loader.load();
+      
+      console.log(docs);
+      return docs
+      }
+    }
+    return NextResponse.json({ success: true });
+} catch (error) {
+   console.log(error) 
+}
+}
