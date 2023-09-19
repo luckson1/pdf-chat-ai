4d49318cddb08d373332035242bafe581652d474
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-19 03:57:07 +0300

    new functions

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { createS3Embeddings, createWebEmbeddings } from "@/inngest/functions";
+import { createAudioEmbeddings, createS3Embeddings, createTxtAws, createWebEmbeddings } from "@/inngest/functions";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(inngest, [createS3Embeddings,  createWebEmbeddings]);
+export const { GET, POST, PUT } = serve(inngest, [createS3Embeddings,  createWebEmbeddings, createAudioEmbeddings, createTxtAws]);
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -2,6 +2,17 @@ import { getPineconeClient } from "@/lib/pinecone-client";
 import { inngest } from "./client";
 import { getChunkedDocsFromS3Files, getChunkedDocsFromWeb } from "@/lib/loaders";
 import { pineconeEmbedAndStore } from "@/lib/vector-store";
+import { Document } from "langchain/document";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { env } from "@/lib/env.mjs";
+import * as AWS from "aws-sdk";
+AWS.config.update({
+  accessKeyId: env.ACCESS_KEY,
+  secretAccessKey: env.SECRET_KEY,
+  region: env.REGION,
+});
+
+const s3 = new AWS.S3();
 
 type s3Prams = {
   data: {
@@ -17,9 +28,24 @@ type webPrams = {
     id:string
   };
 };
+type AudioPrams = {
+  data: {
+  text: string;
+    userId: string;
+    id:string
+  };
+};
+type TextPrams = {
+  data: {
+  text: string;
+   Key: string;
+  };
+};
 export type Events = {
   "docs/s3.create" :  s3Prams;
   "docs/web.create" :  webPrams;
+  "docs/audio.create" :  AudioPrams;
+  "aws/txt.create" : TextPrams
 
 };
 export const createS3Embeddings = inngest.createFunction(
@@ -55,3 +81,49 @@ export const createWebEmbeddings = inngest.createFunction(
     return embeddings;
   }
 );
+
+export const createAudioEmbeddings=inngest.createFunction(
+  { name: "Audio Docs embeddings Created" },
+  { event: 'docs/audio.create'},
+  async ({event, step})=> {
+    const embeddings = await step.run("create embeddings from audio", async () => {
+    const {text, userId, id}=event.data
+    const doc= new Document({
+      pageContent: text,
+     
+    })
+    const docs=[doc]
+    const textSplitter = new RecursiveCharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+    });
+    
+    const chunkedDocs = await textSplitter.splitDocuments(docs);
+    for (const doc of chunkedDocs) {
+      doc.metadata={...doc.metadata, userId, id}
+    }
+    const pineconeClient = await getPineconeClient();
+    
+    await pineconeEmbedAndStore(pineconeClient, chunkedDocs)})
+    return embeddings
+  }
+)
+
+export const createTxtAws=inngest.createFunction(
+  { name: "text file uploaded on AWS" },
+  { event: 'aws/txt.create'},
+  async ({event, step})=> {
+    const embeddings = await step.run("create and upload txt file to AWS", async () => {
+    const {text, Key}=event.data
+    const blob = new Blob([text], { type: "text/plain" });
+    const body = blob.stream();
+    const params = {
+      Bucket: env.BUCKET_NAME,
+      Key,
+      Body: body,
+      ContentType: "text/plain",
+    };
+
+    await s3.upload(params).promise();
+  })}
+)
