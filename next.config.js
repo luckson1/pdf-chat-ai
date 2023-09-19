c831392c5d7b06bccf09bc582b52fe1e51f744a1
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-19 14:31:54 +0300

    fix: remove edge

--- next.config.js
+++ next.config.js
@@ -7,28 +7,28 @@ const nextConfig = {
   },
   swcMinify: true,
   webpack(config, { isServer }) {
- if (!isServer) {
-  config.resolve.fallback = {
-    fs: 'empty',
-    path: false,
-    http: false,
-    https: false,
-    tls: false,
-    net: false,
-    stream: false,
-    os: 'empty',
+//  if (!isServer) {
+//   config.resolve.fallback = {
+//     fs: 'empty',
+//     path: false,
+//     http: false,
+//     https: false,
+//     tls: false,
+//     net: false,
+//     stream: false,
+//     os: 'empty',
 
-    crypto: false,
-    constants: false,
-    dns: false,
-    module: false,
-    zlib : false,
-
-
+//     crypto: false,
+//     constants: false,
+//     dns: false,
+//     module: false,
+//     zlib : false,
 
 
-}
- }
+
+
+// }
+//  }
     config.experiments = { ...config.experiments, topLevelAwait: true, };
     return config;
   },
