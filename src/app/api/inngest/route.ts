fb63c378ff17f4b1b68a50e92669b6f7f5f76d58
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-19 13:18:34 +0300

    fix: allow straming

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,12 @@
+
+export const config = {
+    runtime: "edge",
+  };
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { createAudioEmbeddings, createS3Embeddings, createTxtAws, createWebEmbeddings } from "@/inngest/functions";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(inngest, [createS3Embeddings,  createWebEmbeddings, createAudioEmbeddings, createTxtAws]);
+export const { GET, POST, PUT } = serve(inngest, [createS3Embeddings,  createWebEmbeddings, createAudioEmbeddings, createTxtAws], {
+    streaming: "allow",
+  });
