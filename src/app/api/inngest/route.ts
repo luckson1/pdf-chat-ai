03f0fa386c04d7347b3086433664e438d520d223
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-19 13:32:16 +0300

    remove streaming

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,13 @@
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import { createAudioEmbeddings, createS3Embeddings, createTxtAws, createWebEmbeddings } from "@/inngest/functions";
 
 // Create an API that serves zero functions
-export const { GET, POST, PUT } = serve(inngest, [createS3Embeddings,  createWebEmbeddings, createAudioEmbeddings, createTxtAws], {
-    streaming: "allow",
-  });
+export const { GET, POST, PUT } = serve(inngest, [createS3Embeddings,  createWebEmbeddings, createAudioEmbeddings, createTxtAws],
+//      {
+//     streaming: "allow",
+//   }
+  
+  );
