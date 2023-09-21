04ff51accb6f50af217101ebbd660e65d29709a6
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-21 05:47:38 +0300

    test

--- next.config.js
+++ next.config.js
@@ -8,22 +8,18 @@ const nextConfig = {
   swcMinify: true,
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true, };
-    config.resolve.fallback = {
-      process: require.resolve('process/browser'),
-      zlib: require.resolve('browserify-zlib'),
-      stream: require.resolve('stream-browserify'),
-      util: require.resolve('util'),
-      buffer: require.resolve('buffer'),
-      asset: require.resolve('assert'),
-    }
-    config.externals.push({ sharp: 'commonjs sharp', canvas: 'commonjs canvas' })
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-        process: 'process/browser',
+    config.externals.push({
+      sharp: "commonjs sharp",
+      canvas: "commonjs canvas",
       })
-    )
-    return config;
+      config.plugins.push(
+      new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+      process: "process/browser",
+      })
+      )
+      
+      return config;
   },
 };
 
