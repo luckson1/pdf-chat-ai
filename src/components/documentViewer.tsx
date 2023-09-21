fcfd7e2f0bd5a96a7542fc822c2a8da65b2de013
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-21 17:06:22 +0300

    test

--- src/components/documentViewer.tsx
+++ src/components/documentViewer.tsx
@@ -10,6 +10,7 @@ import { DocumentViewer } from "./s3Loader";
 export function ViewLoader({ id }: { id:  string}) {
   const {data}=api.documents.getUrlInfo.useQuery({id})
 const signedUrl=data?.signedUrl
+console.log(signedUrl)
 const type=data?.type
 if(!signedUrl) return null
 if(!type) return null
