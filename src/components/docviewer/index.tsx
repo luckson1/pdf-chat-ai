e4858bf86fb3da8f390dd47ee2a569310aab9881
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-21 07:23:38 +0300

    height

--- src/components/docviewer/index.tsx
+++ src/components/docviewer/index.tsx
@@ -33,7 +33,7 @@ const DocViewer: FC<DocViewerProps> = (props) => {
     <AppProvider {...props}>
         <div
           id="react-doc-viewer"
-         className="flex flex-col overflow-hidden w-full h-auto"
+         className="flex flex-col overflow-hidden w-full h-full"
           {...props}
         >
           <HeaderBar />
