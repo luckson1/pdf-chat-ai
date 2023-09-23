3a8cbd4e03422e5a30991275617f2ad6cd9a8054
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-23 17:54:42 +0300

    save user input

--- src/components/aiChat.tsx
+++ src/components/aiChat.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { scrollToBottom,  getSources } from "@/lib/utils";
+import { scrollToBottom, getSources } from "@/lib/utils";
 import { ChatLine } from "./chat-line";
 import { useChat, Message } from "ai/react";
 import { Input } from "./ui/input";
@@ -10,29 +10,38 @@ import { useEffect, useRef } from "react";
 import { ChatGPTAgent } from "@/types";
 import { api } from "@/app/api/_trpc/client";
 
-export function Chat({id}: {id: string}) {
+export function Chat({ id }: { id: string }) {
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const {data: savedMessages, isSuccess}=api.messages.getDocumentMessages.useQuery({id })
-  const { messages, input, handleInputChange, handleSubmit, isLoading, data , } =
+  const { data: savedMessages, isSuccess } =
+    api.messages.getDocumentMessages.useQuery({ id });
+  const { messages, input, handleInputChange, handleSubmit, isLoading, data } =
     useChat({
       initialMessages: savedMessages,
-      body: {id},
-      onFinish: (message)=> {
-        saveMessage({role: message.role, content: message.content, documentId: id, sources: getSources(data, message.role, messages.length )})
-        // addToHistory({documentId: id, question: message.})
-      }
-    
+      body: { id },
+      onFinish: (message) => {
+        saveMessage({
+          role: message.role,
+          content: message.content,
+          documentId: id,
+          sources: getSources(data, message.role, messages.length),
+        });
+      },
     });
-const sources= (role: ChatGPTAgent, index: number)=> getSources(data, role, index) 
+  const sources = (role: ChatGPTAgent, index: number) =>
+    getSources(data, role, index);
   useEffect(() => {
     setTimeout(() => scrollToBottom(containerRef), 100);
   }, [messages]);
 
+  const { mutate: saveMessage } = api.messages.create.useMutation();
 
-
-const {mutate: saveMessage}=api.messages.create.useMutation()
-
-
+  const extendedHnadleSubmit = (
+    e: React.FormEvent<HTMLFormElement>,
+    input: string
+  ) => {
+    handleSubmit(e);
+    saveMessage({ role: "user", content: input, documentId: id });
+  };
   return (
     <div className="rounded-2xl border h-[75vh] flex flex-col justify-between">
       <div className="p-6 overflow-auto" ref={containerRef}>
@@ -45,9 +54,12 @@ const {mutate: saveMessage}=api.messages.create.useMutation()
             sources={sources(role, index) ?? []}
           />
         ))}
-      </div>       
+      </div>
 
-      <form onSubmit={handleSubmit} className="p-4 flex clear-both">
+      <form
+        onSubmit={(e) => extendedHnadleSubmit(e, input)}
+        className="p-4 flex clear-both"
+      >
         <Input
           value={input}
           placeholder={"Type to chat with AI..."}
