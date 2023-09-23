7d8a83fcf079351a345cf466f3d5e58fea9efd56
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-24 02:33:54 +0300

    fix: try single page

--- src/components/docviewer/plugins/pdf/components/pages/PDFPages.tsx
+++ src/components/docviewer/plugins/pdf/components/pages/PDFPages.tsx
@@ -29,7 +29,7 @@ const PDFPages: FC<{}> = () => {
       loading={<span>Loading docs... </span>}
     >
       
-      {paginated ? <PDFSinglePage /> : <PDFAllPages />}
+      {<PDFSinglePage /> }
     </Document>
   );
 };
