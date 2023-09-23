7c4b342fd1abac79f01dd437b50987b86fba6956
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-24 01:23:10 +0300

    debug adding sources

--- src/components/aiChat.tsx
+++ src/components/aiChat.tsx
@@ -23,7 +23,7 @@ export function Chat({ id }: { id: string }) {
       body: { id },
       onFinish: (message) => {
         const newSources = data?.at(data?.length - 1)?.sources as string[] | undefined;
-
+console.log(newSources)
         const newMessage = { ...message, sources: newSources };
         saveMessage({
           role: newMessage.role,
