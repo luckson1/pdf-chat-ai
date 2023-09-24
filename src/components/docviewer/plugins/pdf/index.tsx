a23b6bbd3157566ce3e3d117c4d8b3f41329d190
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-25 01:49:38 +0300

    flex basis-1

--- src/components/docviewer/plugins/pdf/index.tsx
+++ src/components/docviewer/plugins/pdf/index.tsx
@@ -16,7 +16,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/$
 const PDFRenderer: DocRenderer = ({ mainState }) => {
   return (
     <PDFProvider mainState={mainState}>
-      <div className="flex flex-col overflow-y-auto">
+      <div className="flex flex-col  flex-1 overflow-y-auto">
         <PDFControls />
         <PDFPages />
       </div>
