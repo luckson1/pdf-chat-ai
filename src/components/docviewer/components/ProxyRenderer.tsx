cbde02289b2829f50e70162c330eb18da34e5f37
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-25 02:06:22 +0300

    hide the overflow

--- src/components/docviewer/components/ProxyRenderer.tsx
+++ src/components/docviewer/components/ProxyRenderer.tsx
@@ -50,7 +50,7 @@ export const ProxyRenderer: FC<{}> = () => {
 
   return (
     <div className="  flex w-full h-full
-    flex-1 overflow-y-auto">
+    flex-1 overflow-hidden">
       <Contents />
     </div>
   );
