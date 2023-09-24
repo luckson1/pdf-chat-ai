299933ad936eb2b3f0d31dea091a8abb5d6eedb8
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-24 03:56:16 +0300

    try

--- next.config.js
+++ next.config.js
@@ -8,16 +8,16 @@ const nextConfig = {
   swcMinify: true,
   webpack(config, {webpack}) {
     config.experiments = { ...config.experiments, topLevelAwait: true, };
-    // config.externals.push({
-    //   sharp: "commonjs sharp",
-    //   canvas: "commonjs canvas",
-    //   })
-    //   config.plugins.push(
-    //   new webpack.ProvidePlugin({
-    //   // Buffer: ["buffer", "Buffer"],
-    //   // process: "process/browser",
-    //   })
-    //   )
+    config.externals.push({
+      sharp: "commonjs sharp",
+      // canvas: "commonjs canvas",
+      })
+      config.plugins.push(
+      new webpack.ProvidePlugin({
+      // Buffer: ["buffer", "Buffer"],
+      // process: "process/browser",
+      })
+      )
     config.module.rules.push({
           test: /\.node/,
          use: 'raw-loader',
