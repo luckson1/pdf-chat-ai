f591780acf1c96fe7d44de5408697942d3211a88
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-24 03:27:13 +0300

    fix: change worker

--- src/components/docviewer/plugins/pdf/index.tsx
+++ src/components/docviewer/plugins/pdf/index.tsx
@@ -6,7 +6,8 @@ import { DocRenderer } from "../../types";
 import PDFPages from "./components/pages/PDFPages";
 import PDFControls from "./components/PDFControls";
 import { PDFProvider } from "./state";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 const PDFRenderer: DocRenderer = ({ mainState }) => {
   return (
     <PDFProvider mainState={mainState}>
