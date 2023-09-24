2b6e6592904661f35898fbeecdf43d6d4715198c
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-24 04:23:46 +0300

    again

--- next.config.js
+++ next.config.js
@@ -10,18 +10,15 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true, };
     config.externals.push({
       sharp: "commonjs sharp",
-      canvas: "commonjs canvas",
+      // canvas: "commonjs canvas",
       })
       config.plugins.push(
       new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-      process: "process/browser",
+      // Buffer: ["buffer", "Buffer"],
+      // process: "process/browser",
       })
       )
-    // config.module.rules.push({
-    //       test: /\.node/,
-    //      use: 'raw-loader',
-    //     });
+      
       return config;
   },
 };
