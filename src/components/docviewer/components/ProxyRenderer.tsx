f337ae7926442c7794305cb8d0fb99730ccc2ff7
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-25 02:07:41 +0300

    use flex-1 instead

--- src/components/docviewer/components/ProxyRenderer.tsx
+++ src/components/docviewer/components/ProxyRenderer.tsx
@@ -49,8 +49,8 @@ export const ProxyRenderer: FC<{}> = () => {
   };
 
   return (
-    <div className="  flex w-full h-full
-    flex-1 overflow-hidden">
+    <div className="
+    flex-1 overflow-y-auto">
       <Contents />
     </div>
   );
