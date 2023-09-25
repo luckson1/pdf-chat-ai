9b1773dd966af5a92399c59bdcbd685f38b93b01
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-25 03:05:54 +0300

    text-white

--- src/components/docviewer/components/ProxyRenderer.tsx
+++ src/components/docviewer/components/ProxyRenderer.tsx
@@ -50,7 +50,7 @@ export const ProxyRenderer: FC<{}> = () => {
 
   return (
     <div className="
-    flex-1 overflow-hidden">
+    flex-1 overflow-hidden text-white">
       <Contents />
     </div>
   );
