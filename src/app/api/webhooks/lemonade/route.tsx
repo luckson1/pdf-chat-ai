0fc0bf73bd31b5ac4a53330c35842a0d9d3eb3c0
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-28 16:50:17 +0300

    add subscription data

--- src/app/api/webhooks/lemonade/route.tsx
+++ src/app/api/webhooks/lemonade/route.tsx
@@ -6,26 +6,111 @@ import { env } from "@/lib/env.mjs";
 import { NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
-export interface LemonResponse {
-  data: {
-    id: string;
-    attributes: {
-      first_order_item: {
+type SubscriptionResponse = {
+    data: {
+      id: string;
+      type: string;
+      links: {
+        self: string;
+      };
+      attributes: {
+        urls: {
+          customer_portal: string;
+          update_payment_method: string;
+        };
+        pause: string | null;
+        status: string;
+        ends_at: string | null;
+        order_id: number;
+        store_id: number;
+        cancelled: boolean;
+        renews_at: string;
+        test_mode: boolean;
+        user_name: string;
+        card_brand: string;
+        created_at: string;
         product_id: number;
+        updated_at: string;
+        user_email: string;
         variant_id: number;
+        customer_id: number;
         product_name: string;
         variant_name: string;
+        order_item_id: number;
+        trial_ends_at: string | null;
+        billing_anchor: number;
+        card_last_four: string;
+        status_formatted: string;
+        first_subscription_item: {
+          id: number;
+          price_id: number;
+          quantity: number;
+          created_at: string;
+          updated_at: string;
+          is_usage_based: boolean;
+          subscription_id: number;
+        };
+        // ... add more attributes here as needed
       };
-      product_id: number;
-      variant_id: number;
-      product_name: string;
-      variant_name: string;
-      user_name: string;
-      user_email: string;
-      status: string;
+      relationships: {
+        order: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        store: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        product: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        variant: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        customer: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        order_item: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        subscription_items: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        subscription_invoices: {
+          links: {
+            self: string;
+            related: string;
+          };
+        };
+        // ... add more relationships here as needed
+      };
+    };
+    meta: {
+      test_mode: boolean;
+      event_name: string;
+      // ... add more meta properties here as needed
     };
   };
-}
+  
 export  async function POST(req: Request) {
     const rawBody = await getRawBody(Readable.from(Buffer.from(await req.text())));
 
@@ -46,7 +131,7 @@ export  async function POST(req: Request) {
     }
 
     try {
-      const response: LemonResponse = JSON.parse(rawBody.toString());
+      const response: SubscriptionResponse = JSON.parse(rawBody.toString());
 
       const { id, attributes } = response.data;
       const {
@@ -54,42 +139,39 @@ export  async function POST(req: Request) {
         product_name,
         variant_id,
         variant_name,
-
+        renews_at,
         status,
         user_email,
-        first_order_item,
+        first_subscription_item
+        
       } = attributes;
 
-    //   await prisma.subscription.create({
-    //     data: {
-    //       id,
-    //       product_id: product_id ?? first_order_item.product_id,
-    //       product_name: product_name ?? first_order_item.product_name,
-    //       variant_id: variant_id ?? first_order_item.variant_id,
-    //       variant_name: variant_name ?? first_order_item.variant_name,
+      await prisma.subscription.create({
+        data: {
+          id,
+          product_id: product_id, 
+          product_name: product_name,
+          variant_id: variant_id ,
+          variant_name: variant_name,
 
-    //       status,
-    //       user_email,
-    //     },
-    //   });
+          status,
+          user_email,
+        },
+      });
 
-    //   if (status === "paid") {
-    //     await prisma.user.update({
-    //       where: {
-    //         email: user_email,
-    //       },
-    //       data: {
-    //         credits: {
-    //           increment: 1000,
-    //         },
-    //       },
-    //     });
+      if (status === "paid") {
+        await prisma.user.update({
+          where: {
+            email: user_email,
+          },
+          data: {
+            isPro: true,
+          },
+        });
 
-    //     res.status(200).json({ response });
-    //   } else {
-    //     res.status(200).json({ response });
-    //   }
-    console.log(response.data)
+      
+      } 
+ 
     return NextResponse.json({ success: true },{status: 200} );
     } catch (error) {
       console.log(error);
