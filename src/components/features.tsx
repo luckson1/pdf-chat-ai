99b172524bcc76f5c0d4ef0dfe758bf79bde6b71
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-09-29 03:08:13 +0300

    better design

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -4,10 +4,10 @@ import { Card } from './ui/card'
 
 export default function Features() {
   return (
-    <div className=' w-full h-auto  flex flex-col space-y-5 my-16'>
+    <div className=' w-full h-auto  flex flex-col'>
      
-        <Card className='flex-1'>
-        <video controls className="flex-1 rounded-lg">
+        <Card className='flex-1 ring-1 ring-inset ring-foreground/10 p-2 g:p-4 bg-foreground/5 '>
+        <video controls    className='rounded-md bg-background p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-foreground/10'>
                     <source src={'/introv1.1.mp4'} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
