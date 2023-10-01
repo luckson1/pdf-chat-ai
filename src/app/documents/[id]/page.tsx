dd77cd032595fe225e6359c7470ef59e423b60ce
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-01 21:49:13 +0300

    better copy

--- src/app/documents/[id]/page.tsx
+++ src/app/documents/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { Chat } from "@/components/aiChat";
 import { MainDocumentViewer } from "@/components/documentViewer";
 import { Card, CardContent } from "@/components/ui/card";
-import { XIcon } from "lucide-react";
+import { XCircleIcon} from "lucide-react";
 import { useSearchParams } from "next/navigation";
 import { useState } from "react";
 
@@ -24,9 +24,9 @@ export default function Home() {
       <div className="w-full max-w-3xl">
         {!exceededPageCount && <Chat id={id} />}
         {exceededPageCount && (
-          <Card className="w-full h-[75vh]">
-            <CardContent className="flex flex-row space-x-4 justify-center items-center text-destructive">
-              <XIcon className="w-4 h-4" /> Exceeds 25 pages. Please Upgrade to contininue
+          <Card className="w-full h-[85vh]">
+            <CardContent className="flex flex-row space-x-4 justify-center items-center text-destructive w-full h-full text-xl">
+              <XCircleIcon className="w-8 h-8" /> Exceeds 25 pages. Please Upgrade to contininue
             </CardContent>
           </Card>
         )}
