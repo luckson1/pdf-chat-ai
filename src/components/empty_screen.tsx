6fe05cea328f21a40da4d8fe9d1c9ce4aa218ced
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-02 01:59:59 +0300

    fix: copy

--- src/components/empty_screen.tsx
+++ src/components/empty_screen.tsx
@@ -71,7 +71,7 @@ export function EmptyScreen({
   return (
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">AI Chatbot!</h1>
+        <h1 className="mb-2 text-lg font-semibold">Welcome to Chat Paperz!</h1>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
         </p>
