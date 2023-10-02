1a0212e919ef70dd9ffaeccef74bbf1c57790120
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-02 23:47:45 +0300

    add chatpanel

---
+++ src/components/chat_panel.tsx
@@ -0,0 +1,47 @@
+import { type UseChatHelpers } from 'ai/react'
+import { PromptForm } from '@/components/prompt_form'
+
+
+
+export interface ChatPanelProps
+  extends Pick<
+    UseChatHelpers,
+    | 'append'
+    | 'isLoading'
+    | 'input'
+    | 'setInput'
+  > {
+  id?: string
+}
+
+export function ChatPanel({
+  id,
+  isLoading,
+  append,
+  input,
+  setInput,
+}: ChatPanelProps) {
+  return (
+    <div className="fixed inset-x-0 bottom-0 bg-gradient-to-b from-muted/10 from-10% to-muted/30 to-50%">
+   
+      <div className="mx-auto sm:max-w-2xl sm:px-4">
+        
+        <div className="space-y-4 border-t bg-background px-4 py-2 shadow-lg sm:rounded-t-xl sm:border md:py-4">
+          <PromptForm
+            onSubmit={async (value) => {
+              await append({
+                id,
+                content: value,
+                role: 'user'
+              })
+            }}
+            input={input}
+            setInput={setInput}
+            isLoading={isLoading}
+          />
+         
+        </div>
+      </div>
+    </div>
+  )
+}
