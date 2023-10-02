81389c856ef83c91819a75037f68847ecd3a873c
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-02 15:45:44 +0300

    fix auth

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -6,32 +6,18 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 import { signIn } from "next-auth/react";
-import { type LucideProps } from "lucide-react";
 import { siteConfig } from "@/lib/config";
 import { Icons } from "@/components/Icons";
 import { useToast } from "@/components/ui/use-toast";
 import { ToastAction } from "@/components/ui/toast";
 
-type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>;
-const Google = (props: LucideProps) => {
-  return (
-    <svg role="img" viewBox="0 0 24 24" {...props}>
-      <path
-        fill="currentColor"
-        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-      />
-    </svg>
-  );
-};
+type UserAuthFormProps = React.HTMLAttributes<HTMLDivElement>
 
 function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = useState(false);
-  const [emailSent, setEmailSent] = useState(false);
   const { toast } = useToast()
-  const [email, setEmail] = useState("");
 
   const signInWithGoogle = async () => {
   try {
@@ -63,49 +49,14 @@ function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   };
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-  
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          await signInWithEmail(email);
-        }}
-      >
-        <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              disabled={isLoading}
-            />
-          </div>
-          <Button
-            disabled={isLoading}
-          >
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
-          {emailSent && (
-            <p className="text-sm font-medium">
-              A magic link has been sent to your email. Follow it to login
-            </p>
-          )}
-        </div>
-      </form>
+
       <div className="relative">
         <div className="absolute inset-0 flex items-center">
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
           <span className="bg-background text-muted-foreground px-2">
-            Or continue with
+            Continue with
           </span>
         </div>
       </div>
@@ -135,7 +86,7 @@ export default function AuthenticationPage() {
             className="absolute inset-0 bg-cover rounded-md lg:rounded-none lg:rounded-l-md"
             style={{
               backgroundImage:
-                "url(https://res.cloudinary.com/dhciks96e/image/upload/v1691673245/lindsey-savage-p4vru5GkP6s-unsplash_krebsm.jpg)",
+               ` url(${siteConfig.ogImage})`,
             }}
           />
           <div className="relative z-20 flex items-center text-lg font-medium text-slate-700">
@@ -145,10 +96,10 @@ export default function AuthenticationPage() {
               }
               height={50}
               width={50}
-              alt="Pic Craft"
+              alt="Chat Paperz"
               className="mr-2 h-6 w-6"
             />{" "}
-          Pic Craft 
+          Chat Paperz 
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
@@ -160,7 +111,7 @@ export default function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Welcome to Pic Craft
+                Welcome to Chat Paperz
               </h1>
             </div>
             <UserAuthForm />
