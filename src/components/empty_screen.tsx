4efba09ff5c6c4d6123907116cc45cad133c7c2a
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-02 23:19:58 +0300

    better ux

--- src/components/empty_screen.tsx
+++ src/components/empty_screen.tsx
@@ -11,38 +11,41 @@ const summaryMessage = {
   heading: "Summarize the source document",
   message: `Summarize all the context and extract any key points with reasoning. Highlight the core argument, and evidence`,
 };
-function QuestionsButtonGroup(props: { content: string ,   setInput: Dispatch<SetStateAction<string>>;}) {
-    // Check if the content has question marks
-    if (!props.content.includes('?')) {
-      return null; // Return null (or any other appropriate JSX) if no questions are detected
-    }
-  
-    // Splitting the content based on question marks
-    const questions = props.content.split('?')
-      .map(question => 
-        // Remove common bullet point characters, numbers followed by dots, and trim spaces
-        question + '?'
-      ) 
-      // Filter out any non-questions or empty strings
-      .filter(question => question.endsWith('?') && question.length > 1); 
-  
-    return (
-        <div className="mt-4 flex flex-col items-start">
-        {questions.map((question, index) => (
-        <Button
-        key={index}
-        variant="link"
-        className="h-auto p-0 text-base mb-4 flex flex-row justify-start items-center text-start text-primary"
-        onClick={() => props.setInput(question)}
-      >
-
-          <ChatMessage message=  {question} />
-      
-      </Button>
-        ))}
-      </div>
-    );
+function QuestionsButtonGroup(props: {
+  content: string;
+  setInput: Dispatch<SetStateAction<string>>;
+}) {
+  // Check if the content has question marks
+  if (!props.content.includes("?")) {
+    return null; // Return null (or any other appropriate JSX) if no questions are detected
   }
+
+  // Splitting the content based on question marks
+  const questions = props.content
+    .split("?")
+    .map(
+      (question) =>
+        // Remove common bullet point characters, numbers followed by dots, and trim spaces
+        question + "?"
+    )
+    // Filter out any non-questions or empty strings
+    .filter((question) => question.endsWith("?") && question.length > 1);
+
+  return (
+    <div className="mt-4 flex flex-col items-start">
+      {questions.map((question, index) => (
+        <Button
+          key={index}
+          variant="link"
+          className="h-auto p-0 text-base mb-4 flex flex-row justify-start items-center text-start text-primary"
+          onClick={() => props.setInput(question)}
+        >
+          <ChatMessage message={question} />
+        </Button>
+      ))}
+    </div>
+  );
+}
 
 export function EmptyScreen({
   setInput,
@@ -51,35 +54,40 @@ export function EmptyScreen({
   setInput: Dispatch<SetStateAction<string>>;
   id: string;
 }) {
-  const {toast}=useToast()
+  const { toast } = useToast();
   const { messages, append } = useChat({
     body: { id },
     onError(message) {
       toast({
         description: `Something went wrong ${message.message}`,
-        variant:'destructive',
+        variant: "destructive",
         action: <ToastAction altText="Try again">Try Again</ToastAction>,
-      })
-      },
-      onResponse(response) {
-        if(response?.status===429) {
-          toast({
-            description: `You have exceeded your daily limit. Please upgrade`,
-            variant:'destructive',
-            action: <ToastAction altText="Try again">Try Again</ToastAction>,
-          })
-        }
-      },
+      });
+    },
+    onResponse(response) {
+      if (response?.status === 429) {
+        toast({
+          description: `You have exceeded your daily limit. Please upgrade`,
+          variant: "destructive",
+          action: <ToastAction altText="Try again">Try Again</ToastAction>,
+        });
+      }
+    },
   });
 
   useEffect(() => {
     const id = nanoid();
-    append({
-      content:
-        "provide summarize the entire context in form of a bullet list of 3 well thought out questions ONLY. The questions should focus on the main concepts covered by the entire context.",
-      id,
-      role: "user",
-    });
+    const content =
+      "provide summarize the entire context in form of a bullet list of 3 well thought out questions ONLY. The questions should focus on the main concepts covered by the entire context.";
+    setTimeout(
+      () =>
+        append({
+          content,
+          id,
+          role: "user",
+        }),
+      1000
+    );
   }, []);
   const getAssistantMessage = useCallback(() => {
     return messages.filter((m) => m.role === "assistant")[0];
@@ -99,15 +107,12 @@ export function EmptyScreen({
             className="h-auto p-0 text-base text-primary"
             onClick={() => setInput(summaryMessage.message)}
           >
- 
-          <ChatMessage message={`- ${summaryMessage.heading}`} />
-            
+            <ChatMessage message={`- ${summaryMessage.heading}`} />
           </Button>
-        
         </div>
         {content && (
-            <QuestionsButtonGroup setInput={setInput} content={content} />
-          )}
+          <QuestionsButtonGroup setInput={setInput} content={content} />
+        )}
       </div>
     </div>
   );
