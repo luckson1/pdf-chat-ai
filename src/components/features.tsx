8f5df68d2d54bf4199e14901d84770ba26d7ffe8
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-03 15:13:53 +0300

    better layout

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -4,7 +4,7 @@ import { Card } from './ui/card'
 
 export default function Features() {
   return (
-    <div className=' w-full max-w-5xl mx-auto h-auto  flex flex-col'>
+    <div className=' w-full max-w-6xl mx-auto h-auto  flex flex-col'>
      
         <Card className='flex-1 ring-1 ring-inset ring-foreground/10 p-2 g:p-4 bg-foreground/5 '>
         <video controls    className='rounded-md bg-background p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-foreground/10'>
