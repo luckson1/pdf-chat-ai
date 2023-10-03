ebe53cb8a67681ed5f2244b80f5359102b175cf0
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-03 03:44:26 +0300

    change video

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -8,7 +8,7 @@ export default function Features() {
      
         <Card className='flex-1 ring-1 ring-inset ring-foreground/10 p-2 g:p-4 bg-foreground/5 '>
         <video controls    className='rounded-md bg-background p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-foreground/10'>
-                    <source src={'/introv1.1.mp4'} type="video/mp4" />
+                    <source src={'/introv1.2.mp4'} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
         </Card>
