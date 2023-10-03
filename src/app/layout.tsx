3744251c982c193d9b14ed3230e030a66371490e
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-04 01:51:11 +0300

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,12 +7,59 @@ import TRPCProvider from "./api/_trpc/Provider";
 import { SiteHeader } from "@/components/site-header";
 import dynamic from 'next/dynamic'
 import { Toaster } from "@/components/ui/toaster";
-import { cn, constructMetadata } from "@/lib/utils";
+import { cn} from "@/lib/utils";
+import { siteConfig } from "@/lib/config";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = constructMetadata()
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}-Students best research tool`,
+  },
+  description: siteConfig.description,
+  keywords: ["chat with pdf", 'chat with docs', 'chat with documents', 'chat pdf ai', 'chat ai'],
+  authors: [
+    {
+      name: "ChatPaperz",
+      url: "https://www.chatpaperz.com/",
+    },
+  ],
+  creator:  "Jack",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@jack",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 type RootLayoutProps = {
   children: React.ReactNode;
