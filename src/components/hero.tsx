7876cef4ec07dafc6be8730d4e5124becc6f05d3
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-07 21:25:24 +0300

    better description

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -26,11 +26,10 @@ export const Hero = () => {
 
            
             
-              <span className="relative"> Get Instant Summaries and Explanations from your documents</span>
+              <span className="relative"> Get Instant Answers and Explanations from your documents</span>
         
           </h1>
-          <p className=" mb-5 text-base  md:text-lg">Save hours reading e-books, journal articles, and lecture notes.</p>
-          <p className=" mb-5 text-base  md:text-lg"> Turn an  audio recording of your lecture or a study session into notes. </p>
+          <p className=" mb-5 text-base  md:text-lg">Let our AI scan your documents and deliver the answers you need. Save hours reading e-books, research papers, and notes</p>
 
           <div className="flex flex-wrap items-center w-full">
             <Button
