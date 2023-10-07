efbcb63833e37abc55cf9dd3a6d2158953e91c4a
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-07 21:41:43 +0300

    border issue

--- src/components/prompt_form.tsx
+++ src/components/prompt_form.tsx
@@ -47,7 +47,7 @@ export function PromptForm({
       }}
       ref={formRef}
     >
-      <div className="relative flex max-h-48 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
+      <div className="relative flex max-h-48 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12 border-t border-secondary md:border-none">
       < TooltipProvider>
         <Tooltip>
           <TooltipTrigger asChild>
