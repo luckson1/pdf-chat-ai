a0c0d376539e0ea58d0a994704ea87f5409be391
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-09 17:58:14 +0300

    improve  alert

--- src/components/alert_dialog_component.tsx
+++ src/components/alert_dialog_component.tsx
@@ -1,32 +1,61 @@
-import { LucideIcon } from 'lucide-react';
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from './ui/alert-dialog';
+import { LucideIcon } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./ui/alert-dialog";
 
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from "react";
+import { buttonVariants } from "./ui/button";
 
-export default function AlertDialogComponent({children, form, description, title, trigger, action}: {children?: ReactNode, form?:string, description:string, title: string,trigger: ReactNode, action: (()=>void) |null}) {
+export default function AlertDialogComponent({
+  children,
+  form,
+  description,
+  title,
+  trigger,
+  action,
+  destructive,
+  actionText,
+}: {
+  children?: ReactNode;
+  form?: string;
+  destructive?: boolean;
+  actionText: string;
+  description: string;
+  title: string;
+  trigger: ReactNode;
+  action: (() => void) | null;
+}) {
   return (
     <AlertDialog>
-    <AlertDialogTrigger>
-{trigger}
-    </AlertDialogTrigger>
-    <AlertDialogContent>
-      <AlertDialogHeader>
-        <AlertDialogTitle>{title}</AlertDialogTitle>
-        <AlertDialogDescription>
-         {description}
-        </AlertDialogDescription>
-      </AlertDialogHeader>
-    {children}
+      <AlertDialogTrigger>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        {children}
         <AlertDialogFooter>
-          <AlertDialogCancel type="button">
-            Cancel
-          </AlertDialogCancel>
-          <AlertDialogAction type="submit" form={form} onClick={action? action: ()=>null}>
-            Save
+          <AlertDialogCancel type="button">Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            type="submit"
+            form={form}
+            onClick={action ? action : () => null}
+            className={buttonVariants({
+              variant: destructive ? "destructive" : undefined,
+            })}
+          >
+            {actionText}
           </AlertDialogAction>
         </AlertDialogFooter>
-      
-    </AlertDialogContent>
+      </AlertDialogContent>
     </AlertDialog>
-  )
+  );
 }
