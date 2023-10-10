0b302acda7b8efa05fce33e092d59c2df2bd790d
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-11 01:07:18 +0300

    node env

--- src/lib/env.mjs
+++ src/lib/env.mjs
@@ -15,6 +15,7 @@ export const env = createEnv({
     PINECONE_ENVIRONMENT: z.string().trim().min(1),
     PINECONE_INDEX_NAME: z.string().trim().min(1),
     DATABASE_URL: z.string().url(),
+    NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
         ? z.string().min(1)
@@ -74,7 +75,8 @@ UPSTASH_REDIS_REST_TOKEN:  z.string()
   UNSD_KEY:process.env. UNSD_KEY,
   ASSEMBLYAI_API_KEY: process.env.ASSEMBLYAI_API_KEY,
   UPSTASH_REDIS_REST_URL:  process.env.UPSTASH_REDIS_REST_URL,
-  UPSTASH_REDIS_REST_TOKEN:  process.env.UPSTASH_REDIS_REST_TOKEN
+  UPSTASH_REDIS_REST_TOKEN:  process.env.UPSTASH_REDIS_REST_TOKEN,
+  NODE_ENV: process.env.NODE_ENV
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
