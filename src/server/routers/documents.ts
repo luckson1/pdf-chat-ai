cfb250d843aa2db75ef69eb059af6e2c205eaa4b
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-11 16:26:33 +0300

    new apis

--- src/server/routers/documents.ts
+++ src/server/routers/documents.ts
@@ -8,25 +8,34 @@ import axios, { AxiosResponse } from "axios";
 import path from "path";
 
 import { TRPCError } from "@trpc/server";
-import { getChunkedDocsFromPDF, getChunkedDocsFromS3Files } from "@/lib/loaders";
+import {
+  getChunkedDocsFromPDF,
+  getChunkedDocsFromS3Files,
+  getChunkedDocsFromWeb,
+  getChunkedDocsFromYT,
+} from "@/lib/loaders";
 import { getPineconeClient } from "@/lib/pinecone-client";
 import { pineconeEmbedAndStore } from "@/lib/vector-store";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { loadSummarizationChain } from "langchain/chains";
+import { PromptTemplate } from "langchain/prompts";
+import { Document } from "langchain/document";
 
-type Uttarance= {
-  confidence:number,
-end:number,
-speaker:string,
-start:number,
-text:string
-words: Array<Word>
-}
-type Word= {
-  text:string,
-start:number,
-end:number,
-confidence:number,
-speaker:string,
-}
+type Uttarance = {
+  confidence: number;
+  end: number;
+  speaker: string;
+  start: number;
+  text: string;
+  words: Array<Word>;
+};
+type Word = {
+  text: string;
+  start: number;
+  end: number;
+  confidence: number;
+  speaker: string;
+};
 export type TranscriptionData = {
   id: string;
   language_model: string;
@@ -48,7 +57,58 @@ AWS.config.update({
 });
 
 const s3 = new AWS.S3();
+const pineconeClient = await getPineconeClient();
+async function fetchBlobFromUrl(signedUrl: string) {
+  try {
+    const response = await fetch(signedUrl);
+    if (!response.ok) {
+      throw new Error("An error occured");
+    }
+    const blob = await response.blob();
 
+    return blob;
+  } catch (err) {
+    throw new Error(`Failed to fetch Blob: ${err}`);
+  }
+}
+const llm = new ChatOpenAI({
+  modelName: "gpt-3.5-turbo-16k",
+  temperature: 0,
+});
+const getSummary = async (docs: Document<Record<string, any>>[]) => {
+  const promptTemplate = `You are an expert in summarizing PDF articles and books.
+Your goal is to create a summary of a pdf.
+The following is a set of documents from the pdf.
+Based on this list of docs, please identify the main themes 
+
+--------
+{text}
+--------
+SUMMARY:`;
+  const qPrompt = PromptTemplate.fromTemplate(promptTemplate);
+  const refineTemplate = `You are an expert in summarizing PDF articles and books.
+Your goal is to create a summary of a pdf.
+We have provided an existing summary up to a certain point: {existing_answer}
+
+Below you find the  pdf text:
+--------
+{text}
+--------
+
+Given the new context, refine the summary. If the context isn't useful, return the original summary.
+Total output will be a summary of the pdf. SUMMARY: `;
+  const refinePrompt = PromptTemplate.fromTemplate(refineTemplate);
+
+  const summarizeChain = loadSummarizationChain(llm, {
+    type: "refine",
+    verbose: true,
+    questionPrompt: qPrompt,
+    refinePrompt: refinePrompt,
+  });
+
+  const summary = await summarizeChain.run(docs);
+  return summary;
+};
 export const documentRouter = createTRPCRouter({
   addDoc: protectedProcedure
     .input(z.object({ key: z.string(), name: z.string(), type: z.string() }))
@@ -63,56 +123,68 @@ export const documentRouter = createTRPCRouter({
             type: input.type,
           },
         });
-        const extention=path.extname(input.name).slice(1)
-        const isPdf=extention==='pdf'
+        const extention = path.extname(input.name).slice(1);
+        const isPdf = extention === "pdf";
         if (isPdf) {
           function generateSignedUrl() {
             const params = {
               Bucket: env.BUCKET_NAME,
-              Key:input.key,
+              Key: input.key,
             };
-    
+
             return s3.getSignedUrl("getObject", params);
           }
           const signedUrl = generateSignedUrl();
-          async function fetchBlobFromSignedUrl(signedUrl: string) {
-            try {
-              const response = await fetch(signedUrl);
-              if (!response.ok) {
-                throw new Error("An error occured");
-              }
-              const blob = await response.blob();
-    
-              return blob;
-            } catch (err) {
-              throw new Error(`Failed to fetch Blob: ${err}`);
-            }
-          }
-          const blob = await fetchBlobFromSignedUrl(signedUrl);
-    if(!blob) {
-      throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Somethingwent  wrong "})
-    }
-          const docs = await getChunkedDocsFromPDF(blob, userId, document.id);
-          if(!docs) {
-            throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Somethingwent  wrong "})
-          }
-          const pineconeClient = await getPineconeClient();
-    
-         await pineconeEmbedAndStore(pineconeClient, docs);
-         return document
-        } else {
-          const pineconeClient = await getPineconeClient();
 
+          const blob = await fetchBlobFromUrl(signedUrl);
+          if (!blob) {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: "Somethingwent  wrong ",
+            });
+          }
+          const docs = await getChunkedDocsFromPDF(blob, userId, document.id);
+          if (!docs) {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: "Somethingwent  wrong ",
+            });
+          }
+
+          await pineconeEmbedAndStore(pineconeClient, docs);
+          const summary = await getSummary(docs);
+          ctx.prisma.document.update({
+            where: {
+              id: document.id,
+            },
+            data: {
+              summary,
+            },
+          });
+          return document;
+        } else {
           const docs = await getChunkedDocsFromS3Files(
             input.key,
             userId,
             document.id
           );
-          if(!docs) {
-            throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "Somethingwent  wrong "})
+          if (!docs) {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: "Somethingwent  wrong ",
+            });
           }
-           await pineconeEmbedAndStore(pineconeClient, docs);
-           return document
+          const summary = await getSummary(docs);
+          ctx.prisma.document.update({
+            where: {
+              id: document.id,
+            },
+            data: {
+              summary,
+            },
+          });
+          await pineconeEmbedAndStore(pineconeClient, docs);
+          return document;
         }
       } catch (error) {
         console.log(error);
@@ -145,7 +217,7 @@ export const documentRouter = createTRPCRouter({
           {
             audio_url: signedUrl,
 
-            speaker_labels: true
+            speaker_labels: true,
           }
         );
         const id = response.data.id;
@@ -173,8 +245,8 @@ export const documentRouter = createTRPCRouter({
         );
         const data = response.data;
         const status = response.data.status;
-    
-        const Key = input.id
+
+        const Key = input.id;
 
         inngest.send({
           name: "docs/audio.create",
@@ -182,30 +254,30 @@ export const documentRouter = createTRPCRouter({
             userId,
             id: input.id,
             name: input.name,
-            Key
+            Key,
           },
         });
         if (status === "completed") {
-       const document= await ctx.prisma.document.findUnique({
-        where: {
-          key:Key
+          const document = await ctx.prisma.document.findUnique({
+            where: {
+              key: Key,
+            },
+          });
+          if (!document) {
+            const newDocument = await ctx.prisma.document.create({
+              data: {
+                name: input.name,
+                key: Key,
+                type: "application/pdf",
+                userId,
+              },
+            });
+            return { ...data, documentId: newDocument.id };
+          }
+          return { ...data, documentId: document.id };
         }
-       })
-if(!document) {
-  const newDocument= await ctx.prisma.document.create({
-    data: {
-      name: input.name,
-      key:Key,
-      type: "application/pdf",
-      userId
-    }
-  })
-  return { ...data, documentId:newDocument.id}
-}
-return { ...data, documentId:document.id}
-        } 
 
-        return { ...data, documentId:undefined}
+        return { ...data, documentId: undefined };
       } catch (error) {
         console.log(error);
       }
@@ -223,37 +295,178 @@ return { ...data, documentId:document.id}
           type: "html",
         },
       });
-      inngest.send({
-        name: "docs/web.create",
-        data: { url: input.url, userId: document.userId, id: document.id },
+      const docs = await getChunkedDocsFromWeb(input.url, userId, document.id);
+      await pineconeEmbedAndStore(pineconeClient, docs);
+
+      const promptTemplate = `You are an expert in summarizing online articles.
+Your goal is to create a summary of an online article.
+The following is a set of documents from the online article.
+
+Based on this list of docs, please identify the main themes 
+{text}
+
+SUMMARY:`;
+      const qPrompt = PromptTemplate.fromTemplate(promptTemplate);
+      const refineTemplate = `You are an expert in summarizing online articles.
+Your goal is to create a summary of an online articles.
+We have provided an existing summary up to a certain point: {existing_answer}
+
+Below you find the online article content:
+--------
+{text}
+--------
+
+Given the new context, refine the summary. If the context isn't useful, return the original summary.
+Total output will be a summary of the online article. SUMMARY: `;
+      const refinePrompt = PromptTemplate.fromTemplate(refineTemplate);
+
+      const summarizeChain = loadSummarizationChain(llm, {
+        type: "refine",
+        verbose: true,
+        questionPrompt: qPrompt,
+        refinePrompt: refinePrompt,
+      });
+
+      const summary = await summarizeChain.run(docs);
+      ctx.prisma.document.update({
+        where: {
+          id: document.id,
+        },
+        data: {
+          summary,
+        },
       });
 
       return document;
     }),
+  addYTDoc: protectedProcedure
+    .input(z.object({ url: z.string().url() }))
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const key = nanoid();
+      const document = await ctx.prisma.document.create({
+        data: {
+          key,
+          name: input.url,
+          userId,
+          type: "YT",
+        },
+      });
+      const docs = await getChunkedDocsFromYT(input.url, userId, document.id);
+      await pineconeEmbedAndStore(pineconeClient, docs);
 
-  getAll: protectedProcedure.input(z.object({
-    skip: z.number().optional(),
-    take: z.number().optional(),
-  }),).query(async ({ ctx , input}) => {
-    const userId = ctx.session.user.id;
-    const {skip, take}=input
-    const docs = await ctx.prisma.document.findMany({
-      where: {
-        userId,
-        isDeleted: false
-      },
-      skip,
-      take,
-  include:{
-    Message: {
-      select: {
-      id: true
+      const promptTemplate = `
+      You are an expert in summarizing YouTube videos.
+      Your goal is to create a summary of a podcast.
+      Based on this list of docs, please identify the main themes.
+      Below you find the transcript of a podcast:
+      --------
+      {text}
+      --------
+      
+SUMMARY:`;
+      const qPrompt = PromptTemplate.fromTemplate(promptTemplate);
+      const refineTemplate = `  You are an expert in summarizing YouTube videos.
+      Your goal is to create a summary of a podcast.
+We have provided an existing summary up to a certain point: {existing_answer}
+
+Below you find the transcript of a podcast content:
+--------
+{text}
+--------
+
+Given the new context, refine the summary. If the context isn't useful, return the original summary.
+Total output will be a summary of thetranscript of a podcast. SUMMARY: `;
+      const refinePrompt = PromptTemplate.fromTemplate(refineTemplate);
+
+      const summarizeChain = loadSummarizationChain(llm, {
+        type: "refine",
+        verbose: true,
+        questionPrompt: qPrompt,
+        refinePrompt: refinePrompt,
+      });
+
+      const summary = await summarizeChain.run(docs);
+      ctx.prisma.document.update({
+        where: {
+          id: document.id,
+        },
+        data: {
+          summary,
+        },
+      });
+      return document;
+    }),
+
+  addWebPDF: protectedProcedure
+    .input(z.object({ url: z.string().url() }))
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const key = nanoid();
+      const document = await ctx.prisma.document.create({
+        data: {
+          key,
+          name: input.url,
+          userId,
+          type: "application/json",
+        },
+      });
+
+      const blob = await fetchBlobFromUrl(input.url);
+      if (!blob) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Somethingwent  wrong ",
+        });
       }
-    }
-  }
-    });
-    return docs;
-  }),
+      const docs = await getChunkedDocsFromPDF(blob, userId, document.id);
+      if (!docs) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Somethingwent  wrong ",
+        });
+      }
+
+      await pineconeEmbedAndStore(pineconeClient, docs);
+
+      const summary = await getSummary(docs);
+      ctx.prisma.document.update({
+        where: {
+          id: document.id,
+        },
+        data: {
+          summary,
+        },
+      });
+      return document;
+    }),
+  getAll: protectedProcedure
+    .input(
+      z.object({
+        skip: z.number().optional(),
+        take: z.number().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const { skip, take } = input;
+      const docs = await ctx.prisma.document.findMany({
+        where: {
+          userId,
+          isDeleted: false,
+        },
+        skip,
+        take,
+        include: {
+          Message: {
+            select: {
+              id: true,
+            },
+          },
+        },
+      });
+      return docs;
+    }),
 
   getOne: protectedProcedure
     .input(z.object({ key: z.string() }))
@@ -261,12 +474,12 @@ return { ...data, documentId:document.id}
       const docs = await ctx.prisma.document.findUniqueOrThrow({
         where: {
           key: input.key,
-          isDeleted: false
+          isDeleted: false,
         },
       });
       return docs;
     }),
-    deleteOne: protectedProcedure
+  deleteOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
       const docs = await ctx.prisma.document.update({
@@ -274,12 +487,12 @@ return { ...data, documentId:document.id}
           id: input.id,
         },
         data: {
-          isDeleted: true
-        }
+          isDeleted: true,
+        },
       });
       return docs;
     }),
-    changeName: protectedProcedure
+  changeName: protectedProcedure
     .input(z.object({ name: z.string(), id: z.string() }))
     .mutation(async ({ ctx, input }) => {
       const docs = await ctx.prisma.document.update({
@@ -287,8 +500,8 @@ return { ...data, documentId:document.id}
           id: input.id,
         },
         data: {
-         name: input.name
-        }
+          name: input.name,
+        },
       });
       return docs;
     }),
@@ -298,7 +511,7 @@ return { ...data, documentId:document.id}
       const document = await ctx.prisma.document.findUniqueOrThrow({
         where: {
           id: input.id,
-          isDeleted: false
+          isDeleted: false,
         },
         select: {
           key: true,
@@ -307,8 +520,8 @@ return { ...data, documentId:document.id}
         },
       });
       let signedUrl = "";
-      const type =document.type
-      const name=document.name
+      const type = document.type;
+      const name = document.name;
       if (document.type === "html") {
         signedUrl = document.name;
         return { signedUrl, type };
@@ -327,6 +540,6 @@ return { ...data, documentId:document.id}
       }
 
       signedUrl = generateSignedUrl();
-      return { signedUrl, type , name};
+      return { signedUrl, type, name };
     }),
 });
