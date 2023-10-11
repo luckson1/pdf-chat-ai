309da102915f40fb35a6f1fdadf6803929856626
Jackson Ndiang'ui <91461937+luckson1@users.noreply.github.com>
2023-10-11 16:23:58 +0300

    url form

--- src/components/webform.tsx
+++ src/components/webform.tsx
@@ -1,24 +1,106 @@
-import React from 'react'
-import { Input } from './ui/input'
-import { Button } from './ui/button'
-import { Icons } from './Icons'
-import { Globe2Icon, GlobeIcon, YoutubeIcon } from 'lucide-react'
-import { Label } from './ui/label'
+import React from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { z } from "zod";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Icons } from "./Icons";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { api } from "@/app/api/_trpc/client";
+import FormError from "./form_error";
 
+const schema = z.object({
+  url: z.string().nonempty("Field is required").url(),
+  formatOption: z.enum(["onlinePDF", "webPage", "youTube"], {
+    required_error: "You need to select type of content.",
+  }),
+});
+
+type FormData = z.infer<typeof schema>;
 export default function Webform() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    register,
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+  const { mutate: addWebPage } = api.documents.addWebDoc.useMutation();
+  const { mutate: addOnlinePDF } = api.documents.addWebPDF.useMutation();
+  const { mutate: addYT } = api.documents.addYTDoc.useMutation();
+  const handleSubitLink = handleSubmit((data) => {
+    const urltype = data.formatOption;
+    urltype === "onlinePDF"
+      ? addOnlinePDF(data)
+      : urltype === "webPage"
+      ? addWebPage(data)
+      : urltype === "youTube"
+      ? addYT(data)
+      : null;
+  });
+
   return (
-   <form className='w-full  flex flex-col space-y-5 justify-center items-center'>
+    <form
+      className="w-full  flex flex-col space-y-4 justify-center items-start"
+      onSubmit={(e) => handleSubitLink(e)}
+    >
+      <Label>Add either YouTube, web page or link to online PDF</Label>
+      <Controller
+        name="formatOption"
+        control={control}
+        render={({ field: { onChange, value, onBlur } }) => (
+          <RadioGroup
+            className="grid grid-cols-3 gap-x-4"
+            onValueChange={onChange}
+            onBlur={onBlur}
+          >
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="youTube"
+                id="youTube"
+                checked={value === "youTube"}
+              />
+              <Label htmlFor="youtube">
+                <Icons.youtube className="w-7 h-7" />
+              </Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="webPage"
+                id="webPage"
+                checked={value === "webPage"}
+              />
+              <Label htmlFor="webPage">
+                <Icons.website className="w-7 h-7" />{" "}
+              </Label>
+            </div>
 
-  
-    <div aria-label='Add a link to a  web page or You tube' className='flex flex-row space-x-1 justify-center md:justify-between items-center text-xs md:text-sm w-full'><span>Add a web page</span> <GlobeIcon className='text-blue-500' />  <span>or YouTube Video</span> <YoutubeIcon className='text-red-400'/> <span></span> </div>
-   <Input className='w-full max-w-sm md:max-w-sm' placeholder='https://...'/>
- 
-    <Button  className='w-full max-w-sm sm:max-w-sm ' >
-    Add Link
-    </Button>
-    
-
-
-   </form>
-  )
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="onlinePDF"
+                id="onlinePDF"
+                checked={value === "onlinePDF"}
+              />
+              <Label htmlFor="onlinePdf">
+                <Icons.pdf className="w-7 h-7" />
+              </Label>
+            </div>
+          </RadioGroup>
+        )}
+      />
+      {errors?.formatOption?.message && (
+        <FormError error={errors.formatOption.message} />
+      )}
+      <Label>Paste Link</Label>
+      <Input
+        className="w-full max-w-sm md:max-w-sm"
+        placeholder="https://..."
+        {...register("url")}
+      />
+      {errors?.url?.message && <FormError error={errors.url.message} />}
+      <Button className="w-full max-w-sm sm:max-w-sm ">Add Link</Button>
+    </form>
+  );
 }
